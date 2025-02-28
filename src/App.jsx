a69276388b99b68a4038d7d220fe08fa35e3c4bb
Sanojkr4120 <sanoj2003feb11@gmail.com>
2025-02-28 15:10:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='font-bold text-2xl flex justify-center mt-20'>
+      Parent Component
+    </div>
+  )
+}
+
+export default App;
